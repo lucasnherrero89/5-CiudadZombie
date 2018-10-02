23ea676ff26e021ec4e004e5dc04e5a19bf741cb
Lucas Herrero <lucasnherrero@gmail.com>
2018-10-02 20:58:49 -0300

    Ya se puede caminar

--- js/jugador.js
+++ js/jugador.js
@@ -10,8 +10,30 @@ var Jugador = {
   alto: 30,
   velocidad: 10,
   vidas: 5,
-  mover: function() {
-    //aca va la mandanga
+  mover: function(desx, desy, dir) {
+    switch (dir) {
+      case "izq":
+        this.sprite = "imagenes/auto_rojo_izquierda.png";
+        this.ancho = 30;
+        this.alto = 15;
+        break;
+      case "der":
+        this.sprite = "imagenes/auto_rojo_derecha.png";
+        this.ancho = 30;
+        this.alto = 15;
+        break;
+      case "arriba":
+        this.sprite = "imagenes/auto_rojo_arriba.png";
+        this.ancho = 15;
+        this.alto = 30;
+        break;
+      case "abajo":
+        this.sprite = "imagenes/auto_rojo_abajo.png";
+        this.ancho = 15;
+        this.alto = 30;
+    }
+    this.x += desx;
+    this.y += desy;
   },
   perderVida: function() {
     alert(`Perdiste una vida, te quedan ${this.vidas}`);

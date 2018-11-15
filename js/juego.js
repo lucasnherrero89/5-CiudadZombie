2a21389a25c20e96c20cb2361d9fadb2ae3abce9
Lucas <lucasnherrero@gmail.com>
2018-11-15 17:50:07 -0300

    en linux

--- js/juego.js
+++ js/juego.js
@@ -20,7 +20,9 @@ var Juego = {
   obstaculosCarretera: [
     /*Aca se van a agregar los obstaculos visibles. Tenemos una valla horizontal
     de ejemplo, pero podras agregar muchos mas. */
-    new Obstaculo("imagenes/valla_horizontal.png", 70, 430, 30, 30, 1)
+    new Obstaculo("imagenes/valla_horizontal.png", 70, 430, 30, 30, 1),
+    new Obstaculo("imagenes/valla_vertical.png", 140, 430, 30, 30, 1),
+    new Obstaculo("imagenes/valla_vertical.png", 400, 200, 30, 30, 1)
   ],
   /* Estos son los bordes con los que se puede chocar, por ejemplo, la vereda.
    Ya estan ubicados en sus lugares correspondientes. Ya aparecen en el mapa, ya
